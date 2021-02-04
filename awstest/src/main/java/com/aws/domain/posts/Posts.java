dfbd84686ec0d083fe83d90cae090d1a2a4a464a
minsun21 <mspark0429@daum.net>
2021-02-04 22:07:22 +0900

    테스트 성공

---
+++ awstest/src/main/java/com/aws/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.aws.domain.posts;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	private String content;
+
+	private String author;
+
+	@Builder
+	public Posts(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+
+}
---
+++ awstest/src/test/java/com/aws/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,20 @@
+package com.aws.web.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+public class HelloResponseDtoTest {
+	
+	@Test
+	public void 롬복_테스트() {
+		String name="test";
+		int amount=1000;
+		
+		HelloResponseDto dto = new HelloResponseDto(name, amount);
+		
+		assertThat(dto.getName()).isEqualTo(name);
+		assertThat(dto.getAmount()).isEqualTo(amount);
+	}
+
+}
